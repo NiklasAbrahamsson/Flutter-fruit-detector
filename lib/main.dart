da10aa3a34b4642698544eea6f33f242cf8f9b5d
NiklasAbrahamsson <Niklas.abrahamsson@me.com>
2021-02-27 23:09:35 +0100

    Added the files for the project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:catsvsdogs/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IFruit',
+      home: MySplash(),
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
